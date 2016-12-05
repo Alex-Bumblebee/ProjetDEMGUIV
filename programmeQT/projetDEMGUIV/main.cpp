f89559f7ab5773725c0dc44cf3c0c789560ab97c
rlesieur05 <r.lesieur13@gmail.com>
2016-12-05 13:48:30 +0100

    Ajout de l'interface graphique du menu principal, et connexion à la base de données dans le main.cpp, et ajout du qt+= sql dans le .pro

--- programmeQT/projetDEMGUIV/main.cpp
+++ programmeQT/projetDEMGUIV/main.cpp
@@ -1,21 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtSql/QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QSqlDatabase maBase=QSqlDatabase::addDatabase("QMYSQL");
-    maBase.setHostName("");
-    maBase.setUserName("");
-    maBase.setPassword("");
-    maBase.setDatabaseName("");
+    maBase.setHostName("172.29.56.11");
+    maBase.setUserName("userDemenagement");
+    maBase.setPassword("Zmcub03");
+    maBase.setDatabaseName("Demenagement");
     if(maBase.open())
     {
         MainWindow w;
          w.show();
         return a.exec();
     }
-    w.show();
 
     return a.exec();
 }
