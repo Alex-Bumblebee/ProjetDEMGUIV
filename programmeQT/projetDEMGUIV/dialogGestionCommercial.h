ac8acb8f0c6d437ae22ec1618fddde04ae3d5fe5
rlesieur05 <r.lesieur13@gmail.com>
2016-12-09 08:43:07 +0100

    Création de l'interface pour la création de dossier du commercial

---
+++ programmeQT/projetDEMGUIV/dialogGestionCommercial.h
@@ -0,0 +1,22 @@
+#ifndef DIALOGGESTIONCOMMERCIAL_H
+#define DIALOGGESTIONCOMMERCIAL_H
+
+#include <QDialog>
+
+namespace Ui {
+class DialogGestionCommercial;
+}
+
+class DialogGestionCommercial : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit DialogGestionCommercial(QWidget *parent = 0);
+    ~DialogGestionCommercial();
+    
+private:
+    Ui::DialogGestionCommercial *ui;
+};
+
+#endif // DIALOGGESTIONCOMMERCIAL_H
