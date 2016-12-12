82e0e67b192270155cc77191c4e74a7a57b61b4e
NHerve <herve.04@hotmail.fr>
2016-12-12 11:33:29 +0100

    modification dialoggestionmodifier.cpp et dialoggestionmodifier.h

--- programmeQT/projetDEMGUIV/dialoggestionmodifier.cpp
+++ programmeQT/projetDEMGUIV/dialoggestionmodifier.cpp
@@ -33,7 +33,7 @@ void DialogGestionModifier::chargerDemenageur()
         QString dateNaissanceDem=reqDemenageur.value(3).toString();
         QString dateEmbaucheDem=reqDemenageur.value(4).toString();
         QString adresseDem=reqDemenageur.value(5).toString();
-        int idAgenceDem=reqDemenageur.value(6).toInt();
+       // int idAgenceDem=reqDemenageur.value(6).toInt();
         QString idPermisDem=reqDemenageur.value(7).toString();
         int chefDem=reqDemenageur.value(8).toInt();
 
@@ -58,6 +58,35 @@ void DialogGestionModifier::chargerVehicule()
 {
     qDebug()<<"DialogGestionModifier::chargerDemenageur()"<<endl;
 
-    QSqlQuery reqVehicule("");
+    QSqlQuery reqVehicule("select id,immat,modele,dateMiseCirculation,volumeUtile,hayon,couchette,nbPlaceCabine,ptac,frequenceEntretien,idAgence from Vehicule inner join TypeVehicule on Vehicule.idType=TypeVehicule.id where etat='disponible';");
 
+    while(reqVehicule.next())
+    {
+        int idVehicule=reqVehicule.value(0).toInt();
+        QString immatVehicule=reqVehicule.value(1).toString();
+        QString dateMiseCirculationVehicule=reqVehicule.value(3).toString();
+        float volumeUtileVehicule=reqVehicule.value(4).toFloat();
+        bool hayonVehicule=reqVehicule.value(5).toBool();
+        bool couchetteVehicule=reqVehicule.value(6).toBool();
+        int nbPlaceVehicule=reqVehicule.value(7).toInt();
+        QString modeleVehicule=reqVehicule.value(2).toString();
+        QString PTACVehicule=reqVehicule.value(8).toString();
+        int frequenceEntretien=reqVehicule.value(9).toInt();
+       //int idAgenceVehicule=reqVehicule.value(10).toInt();
+
+        Vehicule leVehicule;
+        leVehicule.setVehId(idVehicule);
+        leVehicule.setVehImmat(immatVehicule);
+        leVehicule.setVehDateMiseCirculation(dateMiseCirculationVehicule);
+        leVehicule.setVehVolumeUtile(volumeUtileVehicule);
+        leVehicule.setVehHayon(hayonVehicule);
+        leVehicule.setVehCouchette(couchetteVehicule);
+        leVehicule.setVehNbPlaceCabine(nbPlaceVehicule);
+        leVehicule.setVehType(modeleVehicule);
+        leVehicule.setVehPTAC(PTACVehicule);
+        leVehicule.setVehFreqEntretien(frequenceEntretien);
+       // leVehicule.setVehAgence(idAgenceVehicule);
+
+        vectVehicule.push_back(leVehicule);
+    }
 }
